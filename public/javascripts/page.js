6f42aa148df795a9511ae077b58fd7d34fe88523
Eduardo Jurado <zomby_ej@hotmail.com>
2019-06-06 15:00:17 -0500

    662019

--- public/javascripts/page.js
+++ public/javascripts/page.js
@@ -1,23 +1,30 @@
-function TotalHoras(){
-    let h = parseInt( $(".txtHoras").val()); 
-    let d = parseInt( $(".txtDias").val()); 
-    let t = d* h ;
-    if (isNaN(t))
-        t = 0; 
-    if(t <9 ) {
-        t = "0"+t; 
-    }
-    
-    t = t + ":00"; 
-    
-    $(".TotalStandar").text(t); 
+$(document).ready(function() {
+  let f = new Date();
+  $(".txtHoraActual").text(f);
+  setInterval(function() {
+    let f = new Date();
+    $(".txtHoraActual").text(f);
+  }, 1000 * 30);
+});
+function TotalHoras() {
+  let h = parseInt($(".txtHoras").val());
+  let d = parseInt($(".txtDias").val());
+  let t = d * h;
+  if (isNaN(t)) t = 0;
+  if (t < 9) {
+    t = "0" + t;
+  }
+
+  t = t + ":00";
+
+  $(".TotalStandar").text(t);
 }
 
 $(".txtHoras").change(e => {
-    e.preventDefault();
-    TotalHoras(); 
+  e.preventDefault();
+  TotalHoras();
 });
 $(".txtDias").change(e => {
-    e.preventDefault();
-    TotalHoras(); 
+  e.preventDefault();
+  TotalHoras();
 });
