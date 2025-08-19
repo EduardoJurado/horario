ef34457535d775f98b5c4b823da44d96b7d525af
Eduardo Jurado <ejurado@softsierra.com>
2025-08-19 16:56:28 -0500

    ej

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,39 @@
 var express = require('express');
+const oracledb = require('oracledb');
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Horario' });
+  ////
+  oracledb.getConnection({  
+    user: "CENTRAL00",  
+    password: "SOFT0101$",  
+    connectString: "FSC"  
+}, function(err, connection) {  
+    if (err) {  
+         console.error(err.message);  
+         return;  
+    }  
+    connection.execute( "SELECT * FROM tgen_paises",  
+    [],  
+    function(err, result) {  
+         if (err) {  
+              console.error(err.message);  
+              //doRelease(connection);  
+             // return;  
+             res.status(500).send("Error"); 
+
+         }  
+        
+         console.log(result.metaData);  
+         console.log(result.rows);  
+         //doRelease(connection);  
+         res.render('index', { title: 'Horario' });
+    });  
+});  
+  /////
+ // res.render('index', { title: 'Horario' });
 });
 
 module.exports = router;
